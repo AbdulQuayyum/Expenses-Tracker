2dc17994c1ff90fa07cd7707edff05af3e8f2db1
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-26 12:42:22 +0100

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+import { COLORS, FONTS, SIZES, icons, images } from '../constants';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+};
+
+export default Home;
---
+++ screens/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import Home from './Home';
+
+export {
+    Home,
+};
